55c4ec3c34fc11a51b3496dd3efcb6b99e6c06af
Ushitora Anqou <ushitora@anqou.net>
2020-06-14 20:54:49 +0900

    Test code to write in and read from S3 bucket

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AsynQ
 Yet another storage system running on client side with AWS S3 backend
+
+```
+$ docker pull minio/minio
+$ docker run -p 9000:9000 minio/minio server /data
+# Make a bucket 'buckethoge' in MinIO.
+$ AWS_ACCESS_KEY_ID=minioadmin AWS_SECRET_ACCESS_KEY=minioadmin AWS_BUCKET=buckethoge cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use rusoto_core::Region;
+use rusoto_s3::{GetObjectRequest, PutObjectRequest, S3Client, S3};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let s3client = S3Client::new(Region::Custom {
+        name: "hoge".to_string(),
+        endpoint: "http://localhost:9000".to_string(),
+    });
+    let bucket = env::var("AWS_BUCKET").unwrap().to_string();
+
+    let mut request = PutObjectRequest::default();
+    request.bucket = bucket.clone();
+    request.key = String::from("dir/file.hoge");
+    request.body = Some("piyopiyo".to_string().into_bytes().into());
+    let result = s3client.put_object(request).await.unwrap();
+    println!("{:?}", result);
+
+    let mut request = GetObjectRequest::default();
+    request.bucket = bucket;
+    request.key = String::from("dir/file.hoge");
+    let result = s3client.get_object(request).await.unwrap();
+    println!("{:?}", result);
 }
